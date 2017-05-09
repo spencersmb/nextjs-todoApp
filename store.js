20f575d0e873cdd27ee634c49378b03c647c882e
spencer <spencer.bigum@gmail.com>
2017-05-08 20:46:52 -0400

    r6

--- store.js
+++ store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux'
 import thunkMiddleware from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
+// import { composeWithDevTools } from 'redux-devtools-extension'
 import { todosReducer } from './reducers/todosReducer'
 import { authReducer } from './reducers/authReducer'
 import { jokesReducer } from './reducers/jokesReducer'
